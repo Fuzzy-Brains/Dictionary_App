4cf3852a1e9b6e5c8740967e85d279f7b6e70ad3
Suraj Patel <sp814729@gmail.com>
2022-03-31 13:00:08 +0530

    Final Splash Screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,7 @@
 import 'package:dictionary_app/screens/home_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import 'home.dart';
 
@@ -13,45 +14,26 @@ class Splash extends StatefulWidget{
 
 class _SplashState extends State<Splash> {
   @override
-  void initState() {
-    super.initState();
-    _navigatetohome();
-  }
-
-  _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 1500),(){});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children :[
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.greenAccent,
-            ),
-            Container(
-              child: const Text(
-                'Splash Screen',
-                style:TextStyle(
-                    fontSize: 24,
-                    fontWeight:FontWeight.bold
-                ),
-              ),
-            ),
-          ],
+    return Stack(
+      children:[
+        Container(
+          child : Image.asset('assets/images/logo3.png',)
         ),
-      ),
+        Positioned(
+          child:SplashScreen(
+            seconds: 30,
+            navigateAfterSeconds: new HomeScreen(),
+            //imageBackground: AssetImage('assets/images/bgmain.jpeg'),
+            backgroundColor: Color(0xff2c0834),
+            title: new Text('FluentU',textScaleFactor: 3.5,style: TextStyle(fontWeight: FontWeight.bold,color:Colors.white,decorationColor:Colors.white),),
+            image: new Image.asset('assets/images/main.gif'),
+            loadingText: Text("Loading",style: TextStyle(color: Colors.white),),
+            photoSize: 100.0,
+            loaderColor: Colors.blue,
+          ),
+        )
+      ]
     );
-  }
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty('navigatetohome', _navigatetohome));
   }
 }
